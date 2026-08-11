b88d070058c7fec7fe1af7a620f2e4c0f818b68e
54 <1104879088@qq.com>
2026-08-11 18:52:37 +0800

    release: v0.2.0

--- pickthought.koplugin/_meta.lua
+++ pickthought.koplugin/_meta.lua
@@ -2,5 +2,5 @@ local Text = require("pickthought.text")
 return {
     fullname = "撷思",
     description = Text.tr("Sync WeRead highlights and thoughts to local EPUB copies."),
-    version = "0.1.8",
+    version = "0.2.0",
 }
